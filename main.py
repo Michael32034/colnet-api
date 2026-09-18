4e7b1017d0037540c3b92e4d5b5fe552f97e0196
Mikeinnet <misa42034@email.com>
2026-09-18 09:58:26 +0300

    Add git sustem

---
+++ main.py
@@ -0,0 +1,109 @@
+import json
+import logging
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, TypeAdapter
+from pathlib import Path
+
+
+class Groups(BaseModel):
+    id: int
+    name: str
+    link: str | None
+    subjects: list[str]
+
+
+class Lesson(BaseModel):
+    start: str
+    end: str
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"],
+)
+
+logger = logging.getLogger("uvicorn")
+logger.info("start")
+
+groups_file = Path("./data/group.json")
+schedule_file = Path("./data/schedule.json")
+call_schedule_file = Path("./data/call_schedule.json")
+
+groups = TypeAdapter(list[Groups]).validate_json(groups_file.read_text())
+schedule = TypeAdapter(dict[str, dict[str, list[str]]]).validate_json(
+    schedule_file.read_text()
+)
+call_schedule = TypeAdapter(list[Lesson]).validate_json(call_schedule_file.read_text())
+
+groups_by_id = {group.id: group for group in groups}
+groups_by_name = {group.name: group for group in groups}
+
+
+def found_group_by_id(id: int):
+    try:
+        return groups_by_id[id]
+    except KeyError:
+        return None
+
+
+def found_group_by_name(name: str):
+    try:
+        return groups_by_name[name]
+    except KeyError:
+        return None
+
+
+@app.get("/groups")
+def get_groups():
+    return groups
+
+
+@app.get("/call_schedule")
+def get_call_schedule():
+    return call_schedule
+
+
+@app.get("/group/by_id/{id}")
+def get_group_by_id(id: int):
+    group = found_group_by_id(id)
+    if group:
+        return group
+    else:
+        raise HTTPException(status_code=404, detail=f"Group with id:{id} doesn't exist")
+
+
+@app.get("/group/by_name/{name}")
+def get_group_by_name(name: str):
+    group = found_group_by_name(name)
+    if group:
+        return group
+    else:
+        raise HTTPException(
+            status_code=404, detail=f"Group with name:{name} doesn't exist"
+        )
+
+
+@app.get("/group/schedule/by_id/{id}")
+def get_group_schedule_by_id(id: int):
+    group = found_group_by_id(id)
+    if group:
+        return schedule[group.name]
+    else:
+        raise HTTPException(status_code=404, detail=f"Group with id:{id} doesn't exist")
+
+
+@app.get("/group/schedule/by_name/{name}")
+def get_group_schedule_by_name(name: str):
+    group = found_group_by_name(name)
+    if group:
+        return schedule[group.name]
+    else:
+        raise HTTPException(
+            status_code=404, detail=f"Group with name:{name} doesn't exist"
+        )
